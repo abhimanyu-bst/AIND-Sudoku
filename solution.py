0ef4b8db8c49c0b72cbe64c5c6d1e6e0e040b768
abhimanyu <abhimanyu@bluestacks.com>
2017-02-19 13:41:16 +0530

    Added boilerplate

--- solution.py
+++ solution.py
@@ -1,4 +1,18 @@
 assignments = []
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [(x+y) for x in A for y in B]
+
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 def assign_value(values, box, value):
     """
@@ -22,10 +36,6 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
-
 def grid_values(grid):
     """
     Convert grid into a dict of {square: char} with '123456789' for empties.
@@ -36,7 +46,13 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    grid_dict = {}
+    for box, answer in zip(boxes, grid):
+        if answer is '.':
+            grid_dict[box] = '123456789'
+        else:
+            grid_dict[box] = answer
+    return grid_dict
 
 def display(values):
     """
@@ -44,18 +60,66 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+            for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    one_values = [key for key in values.keys() if len(values[key]) is 1]
+    for value in one_values:
+        for peer in peers[value]:
+                values[peer] = values[peer].replace(values[value], '')
+    return values
 
 def only_choice(values):
-    pass
+    for key, u_lists in units.items():
+        for u_list in u_lists:
+            for digit in values[key]:
+                if all([digit not in values[u] or u == key for u in u_list]):
+                    values[key] = digit
+    return values
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if not values:
+        return False
+    # Choose one of the unfilled squares with the fewest possibilities
+    chosen_box = min(values, key=lambda i: len(values[i]) if len(values[i]) is not 1 else 10)
+    if len(values[chosen_box]) == 1:
+        #print('success')
+        return values
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for digit in values[chosen_box]:
+        values_copy = values.copy()
+        values_copy[chosen_box] = digit
+        result = search(values_copy)
+        if result:
+            return result
     pass
 
 def solve(grid):
